1a70a14bd8b29d75f6c8c070ef0e2922ab65f471
Lundy Orlando <simplyeazy@gmail.com>
2018-09-27 09:40:43 +0700

    Spec 1

--- models/tasks.js
+++ models/tasks.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Tasks.associate = function (models) {
     // associations can be defined here
+    Tasks.belongsTo(models.Employee);
   };
   return Tasks;
 };
