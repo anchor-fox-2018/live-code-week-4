924159ff5f68aeeabc1e397bc97e440734329e93
Seneca Manu <dody@Seneca-Manu.local>
2018-09-27 10:43:38 +0700

    Release 3

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; 
+const port = 65534;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+const model = require('./models/index.js');
+
+const employees = model.employees;
+const tasks = model.tasks;
+const taskemployees = model.taskemployees;
+
