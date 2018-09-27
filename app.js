ff0ae201f53d155c0d8557d6e0f9a62e4204297d
anastasia <anastasia.santoso.id@gmail.com>
2018-09-27 10:24:20 +0700

    Spec 4

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const indexRoute = require('./routes/index.js')
+const employeeRoute = require('./routes/employee.js')
+const taskRoute = require('./routes/task.js')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use('/', indexRoute)
+app.use('/employees', employeeRoute)
+    //app.use('/tasks', taskRoute)
+
+app.listen(3000)
---
+++ controllers/employee.js
@@ -0,0 +1,73 @@
+const Model = require('../models')
+
+class Controller {
+    static showAll(req, res) {
+        Model.Employee.findAll({
+                order: [
+                    ['id', 'DESC']
+                ]
+            })
+            .then(data => res.render('employees/employee', {
+                employees: data
+            }))
+            .catch(err => console.log(err))
+    }
+
+    static readOne(req, res) {
+        Model.Employee.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(data => req.render('employees/edit-employee', {
+                employee: data
+            }))
+            .catch(err => console.log(er))
+    }
+
+    static add(req, res) {
+        let firstName = req.body.firstName
+        let lastName = req.body.lastName
+        let email = req.body.email
+
+        Model.Employee.create({
+                firstName,
+                lastName,
+                email
+            })
+            .then(data => res.redirect('employees/employee'))
+            .catch(err => console.log(err))
+    }
+
+    static edit(req, res) {
+        let id = req.params.id
+        let firstName = req.body.firstName
+        let lastName = req.body.lastName
+        let email = req.body.email
+
+        Model.Employee.update({
+                firstName,
+                lastName,
+                email
+            }, {
+                where: {
+                    id: id
+                }
+            })
+            .then(data => res.redirect('employees/employee'))
+            .catch(err => console.log(err))
+    }
+
+    static delete(req, res) {
+        Model.Employee.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(data => res.redirect('employees/employee'))
+            .catch(err => console.log(err))
+    }
+
+} // end class controller
+
+module.exports = Controller
